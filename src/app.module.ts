1ac1214f8e620bed12d7c90b9c9cd0229f547997
Young <navskh@naver.com>
2023-11-27 21:48:40 +0900

    wip module 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { SignModule } from './sign/sign.module';
 import { CatsController } from './cats/cats.controller';
 import { CatsService } from './cats/cats.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [SignModule],
+  imports: [SignModule, CatsModule],
   controllers: [AppController, CatsController],
   providers: [AppService, CatsService],
 })
