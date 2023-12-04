27cc1034119fc457b715e0f51b9d5646859c0b45
Young <navskh@naver.com>
2023-12-04 20:40:47 +0900

    feat : joi 추가

---
+++ src/pipe/joi-validation.pipe.ts
@@ -0,0 +1,20 @@
+import * as Joi from '@hapi/joi';
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+
+@Injectable()
+export class JoiValidationPipe implements PipeTransform {
+  constructor(private readonly schema: Object) {}
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    const { error } = Joi.validate(value, this.schema);
+    if (error) {
+      throw new BadRequestException('Validation failed');
+    }
+    return value;
+  }
+}
